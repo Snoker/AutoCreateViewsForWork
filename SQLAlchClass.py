9e88bf7b8106601cb0f9f96ba672e46b0be07f5a
hampus.sjoman <hampus.sjoman@sprybi.se>
2022-07-05 21:53:11 +0200

    Initial commit.

---
+++ SQLAlchClass.py
@@ -0,0 +1,37 @@
+
+import sqlalchemy as sql
+import pandas as pd
+from sqlalchemy.engine import URL
+import pyodbc
+
+class SQLServer(object):
+    def __init__(self,driver,server,uid,pwd,database):
+        instance = ''
+        if instance == '':
+            connection_string = "DRIVER={"+driver+"};SERVER="+server+";DATABASE="+database+";UID="+uid+";PWD="+pwd
+        else:
+            connection_string = "DRIVER={"+driver+"};SERVER="+server+";INSTANCE="+instance+";DATABASE="+database+";UID="+uid+";PWD="+pwd
+        connection_url = URL.create("mssql+pyodbc", query ={"odbc_connect": connection_string})
+        self.engine = sql.create_engine(connection_url, fast_executemany=True)
+
+    def writeDataFrame_append(self,table,df,schema='dbo', chunksize = 10000 ):
+        self.engine.connect()
+        df.to_sql(name=table, con=self.engine, if_exists='append', index=False, schema=schema, chunksize = chunksize)
+        #self.engine.Connection.close()   <--------- Deprecated? Auto close for each block now?
+
+    def writeDataFrame_createTable(self,table,df,schema='dbo',index=False, index_label = 'ID', chunksize = 10000 ):
+        self.engine.connect()
+        df.to_sql(name=table, con=self.engine, if_exists='fail', index=index, schema=schema, chunksize = chunksize )
+
+    def writeDataFrame_dropAndCreateTable(self,table,df,schema='dbo',index=False, index_label = 'ID', chunksize = 10000 ):
+        self.engine.connect()
+        df.to_sql(name=table, con=self.engine, if_exists='replace', index=index, schema=schema, chunksize = chunksize )
+
+    def executeCustomSelect(self,query):
+        queryString = sql.text(query)
+        result = self.engine.execute(queryString)
+        return result
+
+    def executeCustomQuery(self,query):
+        queryString = sql.text(query)
+        self.engine.execute(queryString)
---
+++ main.py
@@ -0,0 +1,89 @@
+
+import pandas as pd
+import SQLAlchClass
+
+#####################################################################
+#
+#       Set initial variables. This must be done else the script can not run.
+#
+#####################################################################
+
+fullTableName = 'HS.TestingForView'
+driver='SQL Server Native Client 11.0'
+server='localhost'
+#instance='mssqlserver01'
+uid='viewCreation'
+pwd='viewCreation'
+database='HampusTestZone'
+
+#####################################################################
+#
+#      DO NOT ALTER THE CODE BELOW THIS CODE BLOCK.
+#
+#####################################################################
+
+dummyTableName = 'REPLACETHISWITHDUMMYVIEW'
+tableName = fullTableName[fullTableName.index('.')+1:len(fullTableName)]
+
+SQL_Server = SQLAlchClass.SQLServer(driver,server,uid,pwd,database)
+
+columnInformation =f"""
+    select
+    	c.name,typ.name
+    FROM sys.tables t
+    INNER JOIN sys.schemas s
+    	ON t.schema_id = s.schema_id
+    INNER JOIN sys.columns c
+    	ON c.object_id = t.object_id
+    	INNER JOIN sys.types typ
+    		ON c.system_type_id = typ.system_type_id
+    WHERE s.name + '.' + t.name= '{fullTableName}'
+    AND typ.name != 'sysname'
+"""
+response = SQL_Server.executeCustomSelect(columnInformation)
+df = pd.DataFrame(response)
+
+
+
+createViewQuery = f"""
+CREATE VIEW [dbo].v{tableName} AS (
+    SELECT
+"""
+createDummyTable =f"""
+CREATE TABLE dbo.{dummyTableName}
+(
+
+"""
+
+dfMaxValue = len(df.index) -1
+listOfUsedDataTypes = []
+
+for index ,row in df.iterrows():
+
+    if index == dfMaxValue:
+        if row[1] not in listOfUsedDataTypes:
+            listOfUsedDataTypes.append(row[1])
+            createDummyTable = createDummyTable + f'Dummy{row[1]} {row[1]} null'
+
+        if row[0] == 'nvarchar':
+            createViewQuery = createViewQuery + f"\n\t\t {row[0]} = CASE WHEN TRIM({row[0]}) IS NULL THEN (SELECT Dummy{row[1]} FROM {dummyTableName}) ELSE TRIM({row[0]}) END\n"
+        else:
+            createViewQuery = createViewQuery + f"\n\t\t {row[0]} = CASE WHEN {row[0]} IS NULL THEN (SELECT Dummy{row[1]} FROM {dummyTableName}) ELSE {row[0]} END\n"
+    else:
+
+        if row[1] not in listOfUsedDataTypes:
+            listOfUsedDataTypes.append(row[1])
+            createDummyTable = createDummyTable + f'Dummy{row[1]} {row[1]} null,'
+
+        if row[0] == 'nvarchar':
+            createViewQuery = createViewQuery + f"\n\t\t {row[0]} = CASE WHEN TRIM({row[0]}) IS NULL THEN (SELECT Dummy{row[1]} FROM {dummyTableName}) ELSE TRIM({row[0]}) END,\n"
+        else:
+            createViewQuery = createViewQuery + f"\n\t\t {row[0]} = CASE WHEN {row[0]} IS NULL THEN (SELECT Dummy{row[1]} FROM {dummyTableName}) ELSE {row[0]} END,\n"
+    #print('just printed row')
+
+createDummyTable = createDummyTable + ')'
+createViewQuery = createViewQuery + f'FROM {fullTableName})'
+SQL_Server.executeCustomQuery(createDummyTable)
+SQL_Server.executeCustomQuery(f"DROP VIEW IF EXISTS [dbo].v{tableName}")
+SQL_Server.executeCustomQuery(createViewQuery)
+SQL_Server.executeCustomQuery(f"DROP TABLE dbo.{dummyTableName}")
