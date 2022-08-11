dc646688e00456aa866a5f2904c63c53e810e6f8
hampus.sjoman <hampus.sjoman@sprybi.se>
2022-08-11 10:02:23 +0200

    Added functionality to state which columns are t/f that should become bits.

--- CreateStandardView.py
+++ CreateStandardView.py
@@ -7,15 +7,16 @@ import SQLAlchClass
 #       Set initial variables. This must be done else the script can not run.
 #
 #####################################################################
-#fullTableName = 'MirrorFortnox.Account'
-fullTableName = input('Please provide the source table name in the follwing format: schema.tableName (mirror.account): ')
-targetSchema = input('Please provide the target schema that the view is to be created in (it must exist in the DB): ')
+fullTableName = 'Quinyx.TimePunch'
+#fullTableName = input('Please provide the source table name in the follwing format: schema.tableName (mirror.account): ')
+targetSchema = 'Quinyx'
+#targetSchema = input('Please provide the target schema that the view is to be created in (it must exist in the DB): ')
 driver='SQL Server Native Client 11.0'
 server='localhost'
 #instance='mssqlserver01'
 uid='sqluser'
 pwd='sqluser'
-database='Fortnox'
+database='TimePunch'
 
 #####################################################################
 #
@@ -69,7 +70,26 @@ def addSpacing(columnName,dataType,dummyTableName,maxStringLen,maxDataLen):
     finalstring = f"\n\t\t {firstColumnName} = {InitialPartCaseStatement} {subSelect} {elseSection} END"
     return finalstring
 
-
+def convertTrueFalseToBit(columnName):
+    finalString = ''
+    finalString = f"""\n[{columnName}] = CAST(
+													CASE
+														WHEN
+															LOWER([{columnName}])	= ''
+														THEN
+															NULL
+														WHEN
+															LOWER([{columnName}]) = 'true'
+														THEN
+															1
+														WHEN
+															LOWER([{columnName}]) = 'false'
+														THEN
+															0
+														ELSE
+															0
+														END as BIT)"""
+    return(finalString)
 
 dummyTableName = 'REPLACETHISWITHDUMMYVIEW'
 tableName = fullTableName[fullTableName.index('.')+1:len(fullTableName)]
@@ -92,7 +112,13 @@ columnInformation =f"""
 response = SQL_Server.executeCustomSelect(columnInformation)
 df = pd.DataFrame(response)
 
-
+setOfTrueFalseToBit=[]
+convertTrueFalse = ''
+convertTrueFalse = input('Are there any columns that are true/false columns that should be converted to a bit (Yes/No): ')
+if convertTrueFalse.lower() == 'yes':
+    for index, row in df.iterrows():
+        print(f'{index}: {row[0]}')
+    setOfTrueFalseToBit = input('Please enter the corresponding numbers separated by a comma of the columns that should be converted (1,2,3,4): ')
 
 createViewQuery = f"""
 CREATE VIEW {targetSchema}.v{tableName} AS (
@@ -126,12 +152,20 @@ for index ,row in df.iterrows():
         if row[1] not in listOfUsedDataTypes:
             listOfUsedDataTypes.append(row[1])
             createDummyTable = createDummyTable + f'Dummy{row[1]} {row[1]} null'
-        createViewQuery = createViewQuery + f'{addSpacing(row[0],row[1],dummyTableName,maxColLen,maxDataLen)}\n'
+
+        if str(index) in setOfTrueFalseToBit:
+            createViewQuery = createViewQuery + f'\n{convertTrueFalseToBit(row[0])}\n'
+        else:
+            createViewQuery = createViewQuery + f'{addSpacing(row[0],row[1],dummyTableName,maxColLen,maxDataLen)}\n'
     else:
         if row[1] not in listOfUsedDataTypes:
             listOfUsedDataTypes.append(row[1])
             createDummyTable = createDummyTable + f'Dummy{row[1]} {row[1]} null,'
-        createViewQuery = createViewQuery + f'{addSpacing(row[0],row[1],dummyTableName,maxColLen,maxDataLen)},'
+
+        if str(index) in setOfTrueFalseToBit:
+            createViewQuery = createViewQuery + f'{convertTrueFalseToBit(row[0])},'
+        else:
+            createViewQuery = createViewQuery + f'{addSpacing(row[0],row[1],dummyTableName,maxColLen,maxDataLen)},'
 
 createDummyTable = createDummyTable + ')'
 createViewQuery = createViewQuery + f'FROM {fullTableName})'
